65197ceea7f73021642aee83441a0ae471d48b0b
rahul742639 <rahulsingh@7unique.in>
2025-06-15 13:11:45 +0530

    live url

--- src/component/config/axiosInstance.js
+++ src/component/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.230.18:5000/api/',
+  baseURL: 'https://api.sustylo.com/api/',
 
   headers: {
     'Content-Type': 'application/json',
