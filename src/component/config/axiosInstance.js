74a1c9d6f4abebaee99a63f4ac048ed0402a60d7
rahul7425 <rahuljsrasr1112@gmail.com>
2025-06-27 11:52:36 +0530

    URL

--- src/component/config/axiosInstance.js
+++ src/component/config/axiosInstance.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://api.sustylo.com/api/',
 
+  
+
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${localStorage.getItem('token')}`
