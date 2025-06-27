f82dc6dcdf1f541f80e56d11bceff22c0ee55c1c
rahul7425 <rahuljsrasr1112@gmail.com>
2025-06-27 10:47:38 +0530

    url

--- src/component/config/axiosInstance.js
+++ src/component/config/axiosInstance.js
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
 
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('token')}`    
+    'Authorization': `Bearer ${localStorage.getItem('token')}`
   }
 });
 
